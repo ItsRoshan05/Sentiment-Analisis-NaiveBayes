d102be3b150df25a682d9adb2e8704d560325df7
Dhika <yokim051019@gmail.com>
2024-07-23 12:11:35 +0700

    update header

--- sentimentkomala/resources/views/layouts/header.blade.php
+++ sentimentkomala/resources/views/layouts/header.blade.php
@@ -47,7 +47,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">NiceAdmin</span>
+        <span class="d-none d-lg-block">Sentiment Analisis</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
