38baa9a9dc979c2788a68d059414cbc53163ea14
Dhika <yokim051019@gmail.com>
2024-07-25 11:31:31 +0700

    update route login

--- sentimentkomala/resources/views/auth/register.blade.php
+++ sentimentkomala/resources/views/auth/register.blade.php
@@ -89,7 +89,7 @@
                       <button class="btn btn-primary w-100" type="submit">Create Account</button>
                     </div>
                     <div class="col-12">
-                      <p class="small mb-0">Already have an account? <a href="pages-login.html">Log in</a></p>
+                      <p class="small mb-0">Already have an account? <a href="/login">Log in</a></p>
                     </div>
                   </form>
 
